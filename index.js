c022d97a00826ad49c2f7358fbb1ef7be61d72c9
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-07-01 09:15:48 +0300

    Project is able to find emails from a custom text.

---
+++ index.js
@@ -0,0 +1,11 @@
+import { findEmails } from './lib/regExp/emails'
+
+const text = `
+hey dude, maybe you can contact me later by email or phone.
+here is my 
+company email: john.t@example.com 
+personal email: my.email@gmail.com
+phone: 08123123123`
+
+const emails = findEmails(text)
+console.log(emails)
