f34bd50a58f85dbf386dfa3275cc5db92fae7133
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-07-01 11:42:48 +0300

    Module uses nodejs import / exports rather than EcmaScript 6 import / exports.

--- example.js
+++ example.js
@@ -1,11 +1,15 @@
-import maskEmails from './lib/mask/maskEmails';
+const maskEmails = require('./lib/mask/maskEmails')
 
 const text = `
 hey dude, maybe you can contact me later by email or phone.
 here is my 
 company email: john.t@example.com 
 personal email: my.email@gmail.com
-phone: 08123123123`
+
+My phone numbers are: 
+- +62811203898
+- 0818702203
+- +62217458592`
 
 const output = maskEmails(text)
 
